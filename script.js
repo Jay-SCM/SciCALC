7a18183d37931203052431cccddfb252cd01eb4a
Jay-SCM <John.Farmer1086@gmail.com>
2023-10-21 20:54:39 +0930

    removed eval for the sqr function, by converting to a float and then arith to preform the op

--- script.js
+++ script.js
@@ -3,58 +3,37 @@ const ans = document.querySelector('.ans');
 console.log(actions);
 console.log(ans);
 let expression = '';
-let number=0;
+function calculateSquare() {
+    if (expression === '') {
+        return 0;}
+    const value = parseFloat(expression);
+    return value * value;
+}
 actions.addEventListener('click', (e) => {
     console.log(e.target);
     const value = e.target.dataset['value'];
-
-    if(value !== undefined) {
-
-        if(value === 'ce') {
+    if (value !== undefined) {
+        if (value === 'ce') {
             expression = '';
-            ans.value = 0;
-            return true;
-        }
-        else if(value === 'x^2'){
-            expression =square();
-        }
-
-        else if(value === 'radic'){
-            expression = Math.sqrt(expression);
-        }
-        else if(value === 'log'){
-            expression = Math.log(expression);
-        }
-        else if(value === 'sin'){
-            expression = Math.sin(expression);
-        }
-        else if(value === 'cos'){
-            expression = Math.cos(expression);
-        }
-        else if(value === 'tan'){
-            expression = Math.tan(expression);
-        }
-
-        else if(value === '=') {
-            const answer = eval(expression);
-            expression = answer;
-
-        } else {
-            expression += value;
-        }
-
-        if(expression === undefined) {
+             ans.value = 0;
+        } else if (value === 'x^2') {
+            expression = calculateSquare().toString();
+        } else if (value === 'radic') {
+            expression = Math.sqrt(parseFloat(expression)).toString();
+        } else if (value === 'log') {
+            expression = Math.log(parseFloat(expression)).toString();
+        } else if (value === 'sin') {
+            expression = Math.sin(parseFloat(expression)).toString();
+        } else if (value === 'cos') {
+            expression = Math.cos(parseFloat(expression)).toString();
+        } else if (value === 'tan') {
+            expression = Math.tan(parseFloat(expression)).toString();
+        } else if (value === '=') {
+            expression = eval(expression).toString();
+        } else {expression += value;}
+        if (expression === undefined) {
             expression = '';
-            ans.value = 0;
-        } else {
-            ans.value = expression;
-        }
-
-
-
+             ans.value = 0;
+        } else {ans.value = expression;}
     }
-
 });
-const square =()=> {
-    return eval(expression*expression);
-}
